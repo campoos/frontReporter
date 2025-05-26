8872e68c981fef82949f0a72cd5aa81bb596d594
Campoos <joaosantos20071009@gmail.com>
2025-05-25 22:42:19 -0300

    cadastro e login feitos

---
+++ JS/login.js
@@ -0,0 +1,49 @@
+"use strict"
+
+function pegarDados(){
+    const email = document.getElementById("email").value
+    const senha = document.getElementById("senha").value
+
+    const objeto = {
+        email: email,
+        senha: senha
+    }
+
+    return objeto
+}
+
+
+async function fazerLogin(event){
+    try {
+        event.preventDefault()
+
+        const dadosLogin = pegarDados()
+
+        if(!dadosLogin.email || !dadosLogin.senha){
+            return alert("preencha todos os dados!")
+        }
+
+            const response = await fetch("http://localhost:3030/v1/controle-usuario/login", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(dadosLogin)
+        })
+
+        const result = await response.json()
+
+                console.log(result)
+
+         if (response.status === 200){
+                alert("Login realizado com sucesso")
+                window.location.href = "./PAGES/feed.html"
+            }else {
+                alert("Erro ao logar: " + result.message)
+            } 
+    } catch (error) {
+        alert("erro ao fazer login")
+        console.error("erro ao logar", error)
+    }
+}
+
+document.getElementById("buttonLogin")
+    .addEventListener("click", fazerLogin)
--- JS/registro.js
+++ JS/registro.js
@@ -24,28 +24,33 @@ async function cadastrarUsuario(event){
             return alert("preencha todos os dados!")
         }
 
-        const response = await fetch(`http://seu-backend.com/api/usuarios/validar-email?email=${encodeURIComponent(dadosRegistro.email)}`, {
-            method: "GET",
-            headers: {"Content-Type": "application/json"}
+        const emailValidacao = {
+            email: dadosRegistro.email
+        }
+
+        const response1 = await fetch("http://localhost:3030/v1/controle-usuario/usuario/email", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(emailValidacao)
         })
 
-        const resultEmail = await response.json()
+        const resultEmail = await response1.json()
 
-        if(resultEmail){
+        if(resultEmail.status == true){
             alert("email já cadastrado")
         }else{
             
-            const response = await fetch("link da gi para registrar os dados", {
+            const response2 = await fetch("http://localhost:3030/v1/controle-usuario/usuario", {
                 method: "POST",
                 headers: {"Content-Type": "application/json"},
                 body: JSON.stringify(dadosRegistro)
             })
 
-            const result = await response.json()
+            const result = await response2.json()
 
-            if (result.status === 201 || result.message === "SUCCESS_CREATED_ITEM"){
+            if (response2.status === 201){
                 alert("Cadastro realizado com sucesso")
-                window.location.href = "../PAGES/feed.html"
+                window.location.href = "../index.html"
             }else {
                 alert("Erro ao cadastrar usuário: " + result.message)
             }
